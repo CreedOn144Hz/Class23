7660054a321cb059959c1e2729fbfeea5afadcde
AaronPitta <78482843+AaronPitta@users.noreply.github.com>
2021-05-10 19:58:10 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic: true
+        }
+    this.body = Bodies.rectangle(x,y,width,height,options)
+    World.add(world,this.body)
+
+    this.width=width
+    this.height=height
+
+    }
+
+    display(){
+        rectMode(CENTER);
+        fill("brown")
+        var pos = this.body.position;
+        rect(pos.x,pos.y,this.width,this.height)
+    }
+
+}
